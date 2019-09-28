87cab3915e5f4d9b4dec432dc2582739085ad10e
pashakiz <pashakiz@gmail.com>
2019-09-28 20:35:36 +0300

    all btns of tickets

--- app/src/components/ContentNumbersStart.js
+++ app/src/components/ContentNumbersStart.js
@@ -7,94 +7,33 @@ export default class ContentNumbersStart extends Component {
 
         let body = '';
         if (tabOpen === 'tabNumbers') {
+
+            let buttons = [];
+            for (let i=1; i<=20; i++) {
+                console.log('-',i,'-',i+20,'-',i+40,'-',i+60,'-');
+                buttons.push(
+                    <tr>
+                        <td className="cardstable__td">
+                            <button className="btn btn1 btn_ticket" data-ticket={i}>Билет №{i}</button>
+                        </td>
+                        <td className="cardstable__td">
+                            <button className="btn btn1 btn_ticket" data-ticket={i+20}>Билет №{i+20}</button>
+                        </td>
+                        <td className="cardstable__td">
+                            <button className="btn btn1 btn_ticket" data-ticket={i+40}>Билет №{i+40}</button>
+                        </td>
+                        <td className="cardstable__td">
+                            <button className="btn btn1 btn_ticket" data-ticket={i+60}>Билет №{i+60}</button>
+                        </td>
+                    </tr>
+                );
+            }
+
             body = (
                 <div className="exam__cardstable">
                     <table className="cardstable">
                         <tbody>
-                        <tr>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="1">Билет №1</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="80">Билет №80</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="1">Билет №01</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="80">Билет №80</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="1">Билет №1</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="80">Билет №80</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="1">Билет №01</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="80">Билет №80</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="1">Билет №1</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="80">Билет №80</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="1">Билет №01</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="80">Билет №80</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="1">Билет №1</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="80">Билет №80</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="1">Билет №01</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="80">Билет №80</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="1">Билет №1</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="80">Билет №80</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="1">Билет №01</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="80">Билет №80</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="1">Билет №1</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="80">Билет №80</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="1">Билет №01</button>
-                            </td>
-                            <td className="cardstable__td">
-                                <button className="btn btn1 btn_ticket" data-ticket="80">Билет №80</button>
-                            </td>
-                        </tr>
+                            {buttons}
                         </tbody>
                     </table>
                 </div>
