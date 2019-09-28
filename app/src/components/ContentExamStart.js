7bd9df8f4522ef291cde0762e7bb20456f9550f0
pashakiz <pashakiz@gmail.com>
2019-09-28 21:54:06 +0300

    random ticket num

--- app/src/components/ContentExamStart.js
+++ app/src/components/ContentExamStart.js
@@ -10,7 +10,7 @@ export default class ContentExamStart extends Component {
             body = (
                 <div className="exam__random">
                     <p>На экзамен даётся 20 минут.<br/>Внимательно читайте каждый вопрос и все варианты ответов.<br/>Не торопитесь. Удачи!</p>
-                    <button className="btn btn2" id="exam-random-start">НАЧАТЬ</button>
+                    <button className="btn btn2" onClick={this.handleClick}>НАЧАТЬ</button>
                 </div>
             );
         }
@@ -20,5 +20,15 @@ export default class ContentExamStart extends Component {
                 {body}
             </div>
         )
+    }
+
+    randomInteger(min, max) {
+        let rand = min + Math.random() * (max + 1 - min);
+        return Math.floor(rand);
+    }
+
+    handleClick = () => {
+        let num = this.randomInteger(1, 80);
+        console.log(num);
     }
 }
