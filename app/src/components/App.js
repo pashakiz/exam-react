ed78cd53903dcea946b53b82c3ccffb6fa33b915
pashakiz <pashakiz@gmail.com>
2019-09-28 16:36:42 +0300

    add TabContent

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import questions from '../questions';
 import classNames from 'classnames';
+import TabContent from './TabContent'
 
 class App extends Component {
 
@@ -24,17 +25,6 @@ class App extends Component {
         { active: (this.state.tabOpen === 'tabTopics') }
     );
 
-    let body = '';
-    if (this.state.tabOpen === 'tabExam') {
-        body = <div className="exam__content exam__content_exam active">tab Exam</div>;
-    }
-    if (this.state.tabOpen === 'tabNumbers') {
-        body = <div className="exam__content exam__content_numbers active">tab Numbers</div>;
-    }
-    if (this.state.tabOpen === 'tabTopics') {
-        body = <div className="exam__content exam__content_topics active">tab Topics</div>;
-    }
-
     return (
         <div className="exam">
             <div className="exam__tabs">
@@ -42,9 +32,7 @@ class App extends Component {
                 <div className={TabNumbersClasses} onClick={this.handleTabNumbers}>Билеты по номерам</div>
                 <div className={TabTopicsClasses} onClick={this.handleTabTopics}>Билеты по темам</div>
             </div>
-            <div className="exam__content-box">
-                {body}
-            </div>
+            <TabContent questions={questions} tabOpen={this.state.tabOpen}/>
         </div>
     );
     }
